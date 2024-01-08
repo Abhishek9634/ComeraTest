a2d494870c2e5266dee69108c40dc3e52f1736b4
Abhishek Thapliyal <>
2024-01-09 00:05:17 +0530

    feat(tests): add ui tests

--- ComeraTestUITests/ComeraTestUITests.swift
+++ ComeraTestUITests/ComeraTestUITests.swift
@@ -8,26 +8,35 @@
 import XCTest
 
 final class ComeraTestUITests: XCTestCase {
+    
+    private let app = XCUIApplication()
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        app.launch()
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testArticlesList() throws {
+        waitShortTime(2)
+        let elementsQuery = app.scrollViews.otherElements
+        elementsQuery.staticTexts["By Chelsia Rose Marcius, Robin Shulman Agüeros and Ana Ley"].tap()
+        app.scrollViews.images.firstMatch.swipeDown()
+        waitShortTime(1)
+        app.scrollViews.images.firstMatch.swipeUp()
+        app.navigationBars["Article"].buttons["Articles"].tap()
+    }
+    
+    func testOpenArticleWebPage() throws {
+        waitShortTime(2)
+        let elementsQuery = app.scrollViews.otherElements
+        elementsQuery.staticTexts["By Chelsia Rose Marcius, Robin Shulman Agüeros and Ana Ley"].tap()
+        waitShortTime(1)
+        app.scrollViews.images.firstMatch.swipeDown()
+        elementsQuery.buttons["Full Article"].tap()
     }
 
     func testLaunchPerformance() throws {
